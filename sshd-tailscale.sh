55d37d1d9fa89b529fee1b66628085f0fd556d3f
Tir Avintor <TirAvintor@hotmail.com>
2026-01-30 19:00:12 +0200

    Update sshd-tailscale.sh

--- sshd-tailscale.sh
+++ sshd-tailscale.sh
@@ -25,7 +25,11 @@ echo "[*] Install Tailscale"
 echo
 
 sleep 2
-curl -fsSL https://tailscale.com/install.sh | sh
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+sudo apt-get update
+sudo apt-get install tailscale
 
 # Step 5: Run Tailscale
 echo
